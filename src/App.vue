0a52379d983be66c88a287c9a8e470aa34eeedee
TahjaeC <46690291+TahjaeC@users.noreply.github.com>
2020-12-05 15:22:20 -0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="container">
+  <SearchBar @termChange="onTermChange"></SearchBar>
+  <div class="row">
+<VideoDetail v-bind:video="selectedVideo" />
+<VideoList @videoSelect="onVideoSelect" v-bind:videos="videos"></VideoList>
+<!-- V-bind, anytime the video property updates in the parent, it should automatically update the video list and provides the child with the new videos-->
+<!-- The right hand side video is the property we want to share as it is in the parent -->
+<!-- The left video is the property we want to have show up inside the child -->
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY ='AIzaSyCRg-5pU4WAuXZ6zWI5x4FFqFN8ZPUMUVQ';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data: function(){
+    return {
+      videos:[], selectedVideo:null
+    };
+  },
+  methods: {
+    onVideoSelect(video){
+      // console.log(video);
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios.get('https://www.googleapis.com/youtube/v3/search',{
+        params:{
+          key:API_KEY,
+          type:'video',
+          part: 'snippet',
+          q:searchTerm
+        }
+      }).then(response => {
+        this.videos = response.data.items
+      });
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input @input="onInput"/>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'SearchBar',
+  methods:{
+    onInput: function (event) {
+      //console.log(event.target.value);
+      this.$emit('termChange',event.target.value);
+    }
+  }
+};
+
+</script>
+
+<style lang="css" scoped>
+input{
+  width:75%;
+}
+div{
+  text-align: center;
+  margin:20px;
+}
+</style>
